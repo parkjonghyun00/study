328d55b5a9b4282068f041ba70776f899343c342
parkjonghyun00 <43666040+parkjonghyun00@users.noreply.github.com>
2024-05-09 00:27:42 +0900

    전략패턴 공부

---
+++ design pattern/전략패턴/Behavior/UdpBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class UdpBehavior : Behavior
+{
+    public void Receive()
+    {
+        Console.WriteLine("UDP Receive");
+    }
+
+    public void Send()
+    {
+        Console.WriteLine("UDP Send");
+    }
+}
